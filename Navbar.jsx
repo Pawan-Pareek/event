e7b984c79068f81258e8a941a433780abfff7641
Pawan Pareek <109756605+Pawan-Pareek@users.noreply.github.com>
2023-08-07 22:24:47 +0530

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
                <ul>
                 <li><Link to="./Home">Home</Link></li>
                 <li><Link to="./About">About</Link></li>
-                <li><Link to="./Services">Services</Link></li>
+                <li><Link to="./signup">Sign Up</Link></li>
                 <li><Link to="Contact">Contact</Link></li>
             </ul>
     </header>
