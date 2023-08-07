a16d34cd9e89888e21a313f5da1dd5d623fd4c64
Pawan Pareek <109756605+Pawan-Pareek@users.noreply.github.com>
2023-08-07 22:22:40 +0530

    Update Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from 'react';
 import "./Home.css"
-const Home = () => {
-  return (
-        <>
-        <div className="text">
-          <span className='fire'>NEVER MISS THE SHOW</span>
-        </div>
 
-        <div className='come'>
-          <p> Get personalized events recommendations and stay connected with your favorite Events</p>
-        </div>
-        </>
+export default function home() {
+  return (
+    <>
+      <img src='https://images.unsplash.com/photo-1609102026400-3c0ca378e4c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGxpdmUlMjBjb25jZXJ0fGVufDB8fDB8fHww&w=1000&q=80' alt="" className='home-container'/>
+      <div className="text">
+        <span className='fire'>NEVER MISS THE EVENTS</span>
+      </div>
+
+      <div className='come'>
+        <p> Get personalized events recommendations and stay connected with your favorite Events</p>
+      </div>
+    </>
   )
 }
-
-export default Home
